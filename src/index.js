10138bf1d7b6e0589bb124f061c63915bc6a248b
Vitaliy Yarosh <iarosch@protonmail.com>
2022-02-18 12:13:41 +0300

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
-  
+    let dg_val = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let result = '';
+    if(number < 10) {
+        result = dg_val[number];
+    }
+    return result;
 }
